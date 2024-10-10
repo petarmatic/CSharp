42535e9400da1f3b89458b057ac9426ace3fdead
Petar <petarmatic00@gmail.com>
2024-10-10 21:08:33 +0200

    working

--- Ednevnik/Ednevnik/Ednevnik/Program.cs
+++ Ednevnik/Ednevnik/Ednevnik/Program.cs
@@ -43,5 +43,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors("CorsPolicy");
 
 app.Run();
--- Ednevnik/Frontend/src/constants.js
+++ Ednevnik/Frontend/src/constants.js
@@ -3,4 +3,4 @@ export const RouteNames = {
     UCENIK_PREGLED:'/ucenici'
 }
 
-export const BACKEND_URL = 'https://pmatic-001-site1.ctempurl.com/swagger/index.html'
+export const BACKEND_URL = 'https://pmatic-001-site1.ctempurl.com/api/v1'
